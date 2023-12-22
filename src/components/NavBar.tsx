92c17c678bf25c38243b645007d0c59a6f698d5e
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-12-21 20:26:41 -0500

    Ignore button color warning

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -32,10 +32,11 @@ const NavBar = () => {
     <nav className={'flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'}>
       <div className={'flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'}>
         {navData.map((link, index) => {
+          // @ts-ignore
           return (
             <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent`} href={link.path} key={index}>
               <Tooltip showArrow={true} content={link.name} placement={'right-end'} color={'primary'}>
-                <Button isIconOnly radius="full" color="none">
+                <Button isIconOnly radius="full" color={''}>
                   {React.cloneElement(link.icon, { size: "1.75em" })}
                 </Button>
               </Tooltip>
