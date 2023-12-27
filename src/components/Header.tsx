2d0bdb7ea4feb1af86ece157c6966c21211176a3
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-12-26 22:11:42 -0500

    Simplify code logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 'use client'
 import React, {useState} from "react";
-import Link from 'next/link';
 import {RiInstagramLine, RiGithubLine, RiTwitterLine, RiLinkedinLine} from "react-icons/ri";
-import { Button, Tooltip } from '@nextui-org/react'
+import { Link, Button, Tooltip } from '@nextui-org/react'
 import { motion } from 'framer-motion';
 import {fadeIn} from "../../variants";
 
@@ -20,34 +19,32 @@ const Header = () => {
       <header className={'absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'}>
         <motion.div variants={fadeIn('left', 0.3)} initial={'hidden'} animate={'show'} exit={'hidden'} className={'container mx-auto'}>
           <div className={'flex flex-col lg:flex-row justify-between items-center gap-y-1 py-6'}>
-            <Link href={'/'} className="text-2xl">
+            <Link href={'/'} className="text-2xl text-white">
               <b>Jingwen</b>&nbsp;Shi
             </Link>
             <div className={'flex items-center gap-x-5 text-lg'}>
               {headerData.map((link, index) => {
                 return (
-                  <Link href={link.path} key={link.name} rel="noopener noreferrer" target="_blank">
-                    <Tooltip showArrow={true} content={link.name} placement={'bottom'} color={'primary'}>
-                      <Button isIconOnly radius="full" className={'bg-transparent'}
-                        onMouseEnter={() => setHover(prevHover => {
-                          const newHover = [...prevHover];
-                          newHover[index] = true;
-                          return newHover;
-                        })}
-                        onMouseLeave={() => setHover(prevHover => {
-                          const newHover = [...prevHover];
-                          newHover[index] = false;
-                          return newHover;
-                        })}
-                      >
-                        {React.cloneElement(link.icon, {
-                          className: 'sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl', // Add responsive classes here
-                          color: hover[index] ? '#F13024' : 'white',
-                          style: { transition: 'color 0.25s ease-in-out' }
-                        })}
-                      </Button>
-                    </Tooltip>
-                  </Link>
+                  <Tooltip showArrow={true} content={link.name} key={link.name} placement={'bottom'} color={'primary'}>
+                    <Button isIconOnly radius="full" className={'bg-transparent'} href={link.path} as={Link} isExternal
+                      onMouseEnter={() => setHover(prevHover => {
+                        const newHover = [...prevHover];
+                        newHover[index] = true;
+                        return newHover;
+                      })}
+                      onMouseLeave={() => setHover(prevHover => {
+                        const newHover = [...prevHover];
+                        newHover[index] = false;
+                        return newHover;
+                      })}
+                    >
+                      {React.cloneElement(link.icon, {
+                        className: 'sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl', // Add responsive classes here
+                        color: hover[index] ? '#F13024' : 'white',
+                        style: { transition: 'color 0.25s ease-in-out' }
+                      })}
+                    </Button>
+                  </Tooltip>
                 );
               })}
             </div>
