3a71e7492ccb35340cba1bf9544d9c4ad87b10ea
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-02-27 01:47:37 -0500

    Replace avatar2.png with avatar2.webp

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 const Avatar = () => {
   return (
     <div className={'hidden xl:flex xl:max-w-none'}>
-      <Image src={'/avatar2.png'} width={4096} height={4096} alt={''} className={'translate-z-0 w-full h-full'}></Image>
+      <Image src={'/avatar2.webp'} width={4096} height={4096} alt={''} className={'translate-z-0 w-full h-full'}></Image>
     </div>
   );
 }
